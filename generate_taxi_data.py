7f6d32148ffc34945d1705284bd76d4f388c717f
StivensCh <gschavez@ucundinamarca.edu.co>
2026-09-04 01:20:49 -0500

    feat: add taxi regression dataset

---
+++ generate_taxi_data.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import random
+
+data=[]
+
+
+
+for _ in range (600):
+
+    distance = round(random.uniform(1,30),2)  #this part generates a random distance 
+    cost = 5000 + (2500 * distance) + random.uniform(-1500,1500)
+    data.append({
+        "distancia_km": distance,
+        "costo": round(cost , 0)
+    })
+
+df = pd.DataFrame(data)
+
+df.to_csv("data/taxi_data.csv", index=False)
+
+print("Dataset generated succesfully.")
+print(f"Number of record: {len(df)}")
+print(df.head())
+
+          
+
+
